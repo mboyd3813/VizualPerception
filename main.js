46a105e5926123899cdfc7b9a1a638e2e51a7e10
Marcus Boyd <mboyd3813@gmail.com>
2018-08-15 12:05:04 -0500

    My First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function getImage1() {
+    var img = document.getElementById("subPic1").src;
+    document.getElementById("defaultPic").src = img;
+  }
+   
+  function getImage2() {
+    var img = document.getElementById("subPic2").src;
+    document.getElementById("defaultPic").src = img;
+  }
