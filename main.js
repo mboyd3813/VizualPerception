4e729987965b636a43c614cce3c65787f328f2ce
Marcus Boyd <mboyd3813@gmail.com>
2018-12-04 22:30:30 -0600

    new vizualperception

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function getImage1() {
-    var img = document.getElementById("subPic1").src;
-    document.getElementById("defaultPic").src = img;
-  }
-   
-  function getImage2() {
-    var img = document.getElementById("subPic2").src;
-    document.getElementById("defaultPic").src = img;
-  }
+$(document).ready(function (){
+  $('.button_1').click(function (event){
+    console.log('Click')
+
+    var name = $('.name').val()
+    var email = $('.email').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+    
+    if(name.length > 0) {
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Please Enter Name!</div>') 
+    }
+
+    if(email.length > 5 && email.includes('@') && email.includes('.')) {
+    } else{
+      event.preventDefault()
+      statusElm.append('Email is not Valid!')
+    }
+
+    if(message.length >= 30) {
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Message must be 30char long!</div>')
+    }
+  })
+})
